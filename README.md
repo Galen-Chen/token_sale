a9bdcac8740adb8ca6ccca1f0a51850012250742
Galen Chen <ywcctc@gmail.com>
2018-07-04 11:23:27 -0400

    start to code my own cryptocurrency follow dapp university. smoking test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Token_sale
+follow this video: 
+https://www.youtube.com/watch?v=XdKv5uwEk5A&t=431s
+http://www.dappuniversity.com/articles/code-your-own-cryptocurrency-on-ethereum
---
+++ contracts/DappToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.2;
+
+contract DappToken{
+    //constructor
+    //set the total number of tokens
+    // read the total number of tokens
+    
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+
+    }
+
+
+}
